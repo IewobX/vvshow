9cba3ee15007d7dd9a35c9e2daf892e2c388dc26
StrivingToRunning <724145155@qq.com>
2018-08-18 22:19:32 +0800

    登陆成功 之后 个人主页

--- src/components/Me/personal/Personal.vue
+++ src/components/Me/personal/Personal.vue
@@ -1,28 +1,40 @@
 <template>
-    <div class="personal">
-      {{user}}
+  <div class="personal">
+    <div class="header">个人中心</div>
+    <div class="top">
+      <div class="profile">
+        <img src="" alt="" />
+        <span>{{user.username}}</span>
+      </div>
     </div>
+    <ul class="message_item">
+      <li>我的帐号</li>
+      <li>我的收货地址</li>
+      <li>我的收藏</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'personal',
-  data () {
-    return {
-      user: {}
-    }
-  },
-  mounted () {
-    this.$http.get('/api/user/' + this.userId)
-      .then(response => {
-        console.log(response.body)
-        if (response.body.success) {
-          this.user = response.body.data
-        }
-      })
-  },
-  props: ['userId']
-}
+  import './Personal.less'
+  export default {
+    name: 'personal',
+    data () {
+      return {
+        user: {}
+      }
+    },
+    mounted () {
+      this.$http.get('/api/user/' + this.userId)
+        .then(response => {
+          console.log(response.body)
+          if (response.body.success) {
+            this.user = response.body.data
+          }
+        })
+    },
+    props: ['userId']
+  }
 </script>
 
 <style scoped>
